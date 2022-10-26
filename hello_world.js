b767cc1c5c86da82d1b49f057e68a3df4f204f51
hpashok24 <31617045+hpashok24@users.noreply.github.com>
2022-10-26 16:57:57 +0530

    added port

--- hello_world.js
+++ hello_world.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function (req,res) {
 	var q = url.parse(req.url,true);
@@ -19,7 +19,7 @@ http.createServer(function (req,res) {
 	res.write(data);
 	res.end();
     })
-}).listen(8080);
+}).listen(PORT);
 
 console.log("Server running on 8080...")
 
